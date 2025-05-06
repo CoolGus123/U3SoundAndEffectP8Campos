a7b1788bba2e659c749b7db75a7a27efd21b218d
CoolGus123 <gustavo4022010@gmail.com>
2025-05-06 15:27:35 -0700

    i have finished the game
    i have made all the project and just need to turn it in for credit.

--- P8U3SoundAndEffectCampos/Assets/Scripts/SpawnManager.cs
+++ P8U3SoundAndEffectCampos/Assets/Scripts/SpawnManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    private PlayerController playerControllerScript;
+    public GameObject[] obstaclePrefabs;
     public Vector3 spawnPos = new Vector3(25, 0 ,0 );
     private float startDelay = 2;
     private float repeatRate = 2;
+    private int randomObstacle;
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         
     }
@@ -21,7 +24,12 @@ public class SpawnManager : MonoBehaviour
         
     }
     void SpawnObstacle()
-    { 
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, 
+            obstaclePrefabs[randomObstacle].transform.rotation);
+        }
     }
 }
